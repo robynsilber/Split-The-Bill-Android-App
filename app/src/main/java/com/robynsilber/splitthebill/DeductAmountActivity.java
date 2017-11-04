03d8f6ec4bbb50a703c87835a8fe29dcf446269d
Robyn Silber <rjsilber@gmail.com>
2017-11-04 17:06:18 -0400

    Debugged activity_choose_action. Created DeductAmountActivity.

---
+++ app/src/main/java/com/robynsilber/splitthebill/DeductAmountActivity.java
@@ -0,0 +1,13 @@
+package com.robynsilber.splitthebill;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DeductAmountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_deduct_amount);
+    }
+}
