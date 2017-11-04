e18b42405c6f1dc4f0cd3916f373229cef90fdfe
Robyn Silber <rjsilber@gmail.com>
2017-11-04 19:13:24 -0400

    Bugs remain when returning back to ChooseActionActivity from the DeductAmountActivity

--- app/src/main/java/com/robynsilber/splitthebill/ChooseAction.java
+++ app/src/main/java/com/robynsilber/splitthebill/ChooseAction.java
@@ -1,18 +1,40 @@
 package com.robynsilber.splitthebill;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.TextView;
 
 public class ChooseAction extends AppCompatActivity {
+
+    private Bill mBill;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_action);
+        mBill = getIntent().getParcelableExtra("bill");
+        updateBalanceOnUI();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setContentView(R.layout.activity_choose_action);
+        mBill = getIntent().getParcelableExtra("bill");
+        updateBalanceOnUI();
+    }
+
+    private void updateBalanceOnUI(){
+        TextView textView = (TextView) findViewById(R.id.balance);
+        textView.setText(mBill.getStringBalance());
     }
 
     public void deductAnAmount(View view) {
+        Intent intent = new Intent(this, DeductAmountActivity.class);
+        intent.putExtra("bill", mBill);
+        startActivity(intent);
     }
 
     public void splitRemaining(View view) {
@@ -22,5 +44,8 @@ public class ChooseAction extends AppCompatActivity {
     }
 
     public void cancelAndGoToMain(View view) {
+        mBill.setToZero();
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
