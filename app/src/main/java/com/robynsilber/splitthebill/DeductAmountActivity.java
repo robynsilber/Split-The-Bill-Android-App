bf0b5d5e366a19e4e310fd243f94300dbd6111a9
Robyn Silber <rjsilber@gmail.com>
2017-11-04 18:41:18 -0400

    Added GUI elements to activity_deduct_amount and updated formatting. Added button listeners to DeductAmountActivity.

--- app/src/main/java/com/robynsilber/splitthebill/DeductAmountActivity.java
+++ app/src/main/java/com/robynsilber/splitthebill/DeductAmountActivity.java
@@ -2,12 +2,26 @@ package com.robynsilber.splitthebill;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class DeductAmountActivity extends AppCompatActivity {
+
+    private Bill mBill;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_deduct_amount);
+        mBill = getIntent().getParcelableExtra("bill");
+
+    }
+
+    public void addTaxToAmount(View view) {
+    }
+
+    public void goBackToChooseActionActivity(View view) {
+    }
+
+    public void deductAmountFromBalance(View view) {
     }
 }
