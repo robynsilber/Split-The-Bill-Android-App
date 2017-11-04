2b6993faaa4945e72d32f781dab984971c6f7b6a
Robyn Silber <rjsilber@gmail.com>
2017-11-04 13:15:55 -0400

    Implemented Bill constructor and static methods for validating input obtained from the EditTexts.

--- app/src/main/java/com/robynsilber/splitthebill/Bill.java
+++ app/src/main/java/com/robynsilber/splitthebill/Bill.java
@@ -1,17 +1,31 @@
 package com.robynsilber.splitthebill;
 
-/**
- * Created by robynjsilber on 11/4/17.
- */
 
 public class Bill {
 
     private double mBalance = 0.0;
-    private double mtaxPercent = 0.0;
+    private final double mtaxPercent;
 
-    public Bill(double beforeTaxBalance, double taxes){
-        mBalance = beforeTaxBalance + taxes;
-        mtaxPercent = taxes/beforeTaxBalance;
+    public Bill(String strBeforeTaxBalance, String strTaxes){
+
+        // Check if input contains dollar signs; if so, remove them
+        if(containsDollarSign(strBeforeTaxBalance)){
+            strBeforeTaxBalance = strBeforeTaxBalance.substring(1); // removes dollar sign
+        }
+        if(containsDollarSign(strTaxes)){
+            strTaxes = strTaxes.substring(1); // removes dollar sign
+        }
+
+        // Check if inputs are valid dollar quantities
+        if(isPriceValid(strBeforeTaxBalance) && isPriceValid(strTaxes)){
+            double beforeTaxBalance = Double.parseDouble(strBeforeTaxBalance);
+            double taxes = Double.parseDouble(strTaxes);
+
+            mBalance = beforeTaxBalance + taxes;
+            mtaxPercent = taxes/beforeTaxBalance;
+        }else{
+            mtaxPercent = 0.0;
+        }
     }
 
     public double getBalance(){
@@ -22,6 +36,41 @@ public class Bill {
         if(amount <= mBalance){
             mBalance -= amount;
         }
+    }
+
+
+
+
+
+    /* Checks if user input to edit text contains dollar symbol
+     * Returns true if contains dollar symbol; otherwise false */
+    public static boolean containsDollarSign(String userInput){
+        if(userInput.length() > 0){
+            if(userInput.charAt(0) == '$') return true;
+        }
+        return false;
+    }
+
+    /* Checks if user input to edit text contains a valid dollar quantity:
+     * only numerical digits and no more than one decimal point, and
+     * no more than two digits to the right of the decimal point */
+    public static boolean isPriceValid(String userInput){
+        int count = 0;
+        boolean decimalFound = false;
+        for(int i=0; i<userInput.length(); i++){
+            if(userInput.charAt(i)>=48 && userInput.charAt(i)<=57){
+                if(decimalFound){
+                    count++;
+                    if(count > 2) return false; // more than two digits to the right of decimal
+                }
+            }else if(userInput.charAt(i)==46){
+                if(decimalFound) return false; // multiple decimal points
+                else{
+                    decimalFound = true;
+                }
+            }
+        }
+        return true;
     }
 
 }
--- app/src/main/java/com/robynsilber/splitthebill/MainActivity.java
+++ app/src/main/java/com/robynsilber/splitthebill/MainActivity.java
@@ -3,8 +3,11 @@ package com.robynsilber.splitthebill;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Bill mBill;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +16,17 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void createBill(View view) {
+        EditText balanceEditText = (EditText) findViewById(R.id.balance_EditText);
+        EditText taxesEditText = (EditText) findViewById(R.id.taxes_EditText);
 
+        // instantiates Bill
+        mBill = new Bill(balanceEditText.getText().toString(), taxesEditText.getText().toString());
+
+        // Before starting next Activity, check that balance is greater than zero
+        if(mBill.getBalance() > 0.0){
+            // start next Activity
+        }
     }
+
+
 }
