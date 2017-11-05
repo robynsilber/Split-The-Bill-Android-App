81283f88529827350ab528ec8c3be7f43a467c1f
Robyn Silber <rjsilber@gmail.com>
2017-11-05 08:08:56 -0500

    Fixed bugs in DeductAmountActivity. Updated Person.java to implement Parcelable, Serializable. Updated Parcelable implementation methods in Bill.java w.r.t. AL<Person>

--- app/src/main/java/com/robynsilber/splitthebill/DeductAmountActivity.java
+++ app/src/main/java/com/robynsilber/splitthebill/DeductAmountActivity.java
@@ -1,8 +1,11 @@
 package com.robynsilber.splitthebill;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 public class DeductAmountActivity extends AppCompatActivity {
 
@@ -13,15 +16,56 @@ public class DeductAmountActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_deduct_amount);
         mBill = getIntent().getParcelableExtra("bill");
-
     }
 
-    public void addTaxToAmount(View view) {
+
+    private void goBackToChooseActionActivity(){
+        Intent intent = new Intent(this, ChooseActionActivity.class);
+        intent.putExtra("bill", mBill);
+        startActivity(intent);
     }
 
     public void goBackToChooseActionActivity(View view) {
+        goBackToChooseActionActivity();
     }
 
     public void deductAmountFromBalance(View view) {
+        CheckBox checkBox = (CheckBox) findViewById(R.id.add_tax_checkbox);
+        EditText editTextName = (EditText) findViewById(R.id.enter_name_here_edittext);
+        EditText editTextAmt = (EditText) findViewById(R.id.amount_to_deduct_edittext);
+
+        if(editTextAmt.getText().toString().length() > 0){
+
+            Person person;
+            double amt = Double.parseDouble(editTextAmt.getText().toString());
+
+            if(checkBox.isChecked()){
+                amt += (amt * mBill.getTaxPercent());
+            }
+
+            if(editTextName.getText().toString().length() > 0){
+
+                person = new Person(editTextName.getText().toString(), amt);
+            }else{
+                person = new Person(amt);
+            }
+
+            mBill.deductFromBalance(amt);
+            mBill.addPersonToList(person);
+
+            goBackToChooseActionActivity();
+
+        }else{
+
+            /**
+             * TODO: display error message.
+             * */
+
+        }
+
+    }
+
+    public void checkedBoxToAddTax(View view) {
+
     }
 }
